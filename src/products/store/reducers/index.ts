73d29d2138a10d73f99ac7c39dd58a4d34913ecd
akhila <akhi.prabha@gmail.com>
2017-12-13 15:31:23 +0530

    lazyloading components

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -9,7 +9,7 @@ export const reducers:ActionReducerMap<ProductState>= {
     pizzas:fromPizzas.reducer
 }
 export const getProductState= createFeatureSelector<ProductState>('products');
-debugger
+// debugger
 export const getPizzaState= createSelector(getProductState,
                             (state:ProductState) => state.pizzas );
 export const getAllPizzas= createSelector(getPizzaState, fromPizzas.getPizzas);
